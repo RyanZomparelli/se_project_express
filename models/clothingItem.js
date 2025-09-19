40d9b950babaace2db94f229d163c11edcdc9ea0
Ryan Zomparelli <zomp.ryan@gmail.com>
2025-09-19 16:10:23 -0400

    feat(models): create schemas for user and clothingItem resources and export models for the schemas

---
+++ models/clothingItem.js
@@ -0,0 +1,41 @@
+const mongoose = require("mongoose");
+
+const validator = require("validator");
+
+const clothingItemSchema = new mongoose.Schema({
+  name: {
+    required: true,
+    type: String,
+    minlength: 2,
+    maxlength: 30,
+  },
+  weather: {
+    required: true,
+    type: String,
+    enum: ["hot", "warm", "cold"],
+  },
+  imageUrl: {
+    required: true,
+    type: String,
+    validate: {
+      validator: (url) => validator.isURL(url),
+      message: "This is an invalid url. Please try again.",
+    },
+  },
+  owner: {
+    required: true,
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "user",
+  },
+  likes: {
+    type: [mongoose.Schema.Types.ObjectId],
+    ref: "user",
+    default: [],
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now,
+  },
+});
+
+module.exports = mongoose.model("clothingItem", clothingItemSchema);
---
+++ models/user.js
@@ -0,0 +1,31 @@
+const mongoose = require("mongoose");
+
+/* validator is a node package that contains validation code like isURL() or
+ other quick validation methods. Not to be confused with the validate field in
+ mongoose schemas with its validator property. */
+const validator = require("validator");
+
+/* A schema is a blueprint for the resources that are going to be saved to the database.
+   mongoose uses the schema to validate documents before they're saved. */
+const userSchema = new mongoose.Schema({
+  name: {
+    required: true,
+    type: String,
+    minlength: 2,
+    maxlength: 30,
+  },
+  avatar: {
+    required: true,
+    type: String,
+    //👇 The mongoose schema field
+    validate: {
+      validator: (url) => validator.isURL(url), //👈 The validator library
+      message: "This is an invalid url. Please try again.",
+    },
+  },
+});
+
+/* The model is like a class wrapper for the schema which you can instantiate and
+  and use in your controllers(functions to handle client requests) to
+  interact(Create Read Update Delete) with the database.  */
+module.exports = mongoose.model("user", userSchema);
